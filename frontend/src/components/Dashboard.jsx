1573a3a17ea8411f759d5a59ac0ee08cb0b72112
heraldofortuna <heraldofortuna@gmail.com>
2022-01-20 10:56:20 -0500

    El componente Dashboard fue creado, además que se añadió la dependencia Axios a la aplicación

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Fecha de nacimiento</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Nombre 1</td>
+          <td>Apellido 1</td>
+          <td>Fecha de nacimiento 1</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default Dashboard;
