1f9e5f37be41e8ce8c4650ffaa1e3b97d905931f
heraldofortuna <heraldofortuna@gmail.com>
2022-01-20 00:12:14 -0500

    Se creó la api para crear un cliente nuevo

--- backend/src/routes/clients.js
+++ backend/src/routes/clients.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const mysqlConnection = require("../database");
 
+// API #3: List of clients
 router.get("/", (req, res) => {
   mysqlConnection.query("SELECT * FROM clients", (err, rows, fields) => {
     if (!err) {
@@ -13,6 +14,26 @@ router.get("/", (req, res) => {
   });
 });
 
+// API #1: Add a new client
+router.post("/", (req, res) => {
+  const { id, name, lastname, birthday } = req.body;
+  const query = `
+    CALL clientAddOrEdit(?, ?, ?, ?);
+  `;
+  mysqlConnection.query(
+    query,
+    [id, name, lastname, birthday],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ Status: "Client saved" });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+// EXTRA API: Get a specific client
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   mysqlConnection.query(
