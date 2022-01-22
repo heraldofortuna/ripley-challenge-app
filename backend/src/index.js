88966a623f621f5e19bcb926353e79a48dbb2090
heraldofortuna <heraldofortuna@gmail.com>
2022-01-21 19:23:45 -0500

    Se ccambió la configuración del proyecto backend, además de crear el build para comenzar con el deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,13 @@ const app = express();
 // Settings
 app.set("port", process.env.PORT || 3001);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
 // Middlewares
 app.use(express.json());
 
